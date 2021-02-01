eda3990efacb5e7080478de07897c61d91e13295
Kotova Maryna <marynakotova256@gmail.com>
2021-02-01 10:00:55 +0200

    ua-page

--- #source/js/menu.js
+++ #source/js/menu.js
@@ -1,8 +1,17 @@
-$('.wrapper').addClass('loaded');
+// $('.wrapper').addClass('loaded');
 
-$('.icon-menu').click(function(event){
-   $(this).toggleClass('active');
-   $('.menu__body').toggleClass('active');
-   $('body').toggleClass('lock');
+// $('.icon-menu').click(function(event){
+//    $(this).toggleClass('active');
+//    $('.menu__body').toggleClass('active');
+//    $('body').toggleClass('lock');
+// });
+let body = document.body;
+let burger = document.getElementById('burger');
+let menu = document.getElementById('menu');
+
+burger.addEventListener('click', ()=>{
+   burger.classList.toggle('active');
+   menu.classList.toggle('active');
+   body.classList.toggle('lock');
 });
 
--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,18 @@
 let body = document.body;
 let burger = document.getElementById('burger');
+let burger1 = document.getElementById('burger1');
 let menu = document.getElementById('menu');
+let menu1 = document.getElementById('menu1');
 
 burger.addEventListener('click', ()=>{
    burger.classList.toggle('active');
    menu.classList.toggle('active');
+   body.classList.toggle('lock');
+});
+
+burger1.addEventListener('click', ()=>{
+   burger1.classList.toggle('active');
+   menu1.classList.toggle('active');
    body.classList.toggle('lock');
 });
 
